ca6612571136db007758961958aa580f21a62192
saxgutierrez <diegofdog@gmail.com>
2018-02-02 17:31:04 -0500

    reto 3 de ruby and rails

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,5 @@
 class ActorsController < ApplicationController
+	def index
+		@actors = Actor.all # se utiliza el nombre del model es decir Actor, y se guarda todo el registro en la variable @actors
+	end
 end
